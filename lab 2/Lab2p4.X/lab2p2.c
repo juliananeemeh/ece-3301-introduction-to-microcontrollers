8112772f30d5495de315b387638388f7a36e2c51
juliananeemeh <juliananeemeh@gmail.com>
2026-01-05 11:42:33 -0800

    Add files via upload

---
+++ lab 2/Lab2p4.X/lab2p2.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include <math.h>
+#include <p18f4620.h> 
+
+
+void main()
+{
+char in_sw; // Use variable ?in_sw? as char
+ TRISA = 0x01; // fill out the ?? with the proper values
+ TRISC = 0x00; // fill out the ?? with the proper values
+ ADCON1 = 0x0F; // Set the ADCON1 to this value to force
+ // the pins to be in digital mode
+
+ while (1)
+{
+in_sw = PORTA; // read data from PORTA and save it
+ // into ?in?
+ in_sw = in_sw & 0x07;// Mask out the unused upper four bits
+ // while preserving the lower 3-bits
+ PORTC = in_sw; // Output the data to PORTB
+}
+ }
